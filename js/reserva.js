931017511f2d40d5d1ff64bf40119a5351e67c56
Ali Mohamed <85517447+alimkhodr@users.noreply.github.com>
2024-06-18 23:46:45 -0300

    api to whatsapp

--- js/reserva.js
+++ js/reserva.js
@@ -1,65 +1,47 @@
 function validarDataHora() {
-  var inputNome = encodeURIComponent(document.getElementById('text_nome').value);
-  var inputEvento = encodeURIComponent(document.getElementById('text_evento').value);
-  var inputQtd = encodeURIComponent(document.getElementById('text_qtd').value);
-  var inputHora = document.getElementById('text_hora').value;
-  var inputObs = encodeURIComponent(document.getElementById('text_obs').value);
-
-  var inputData = document.getElementById('text_data').value;
-  var [ano, mes, dia] = inputData.split("-");
-  var dataFormatada = dia + '/' + mes + '/' + ano;
-
-  var currentDate = new Date(); // Data e hora atuais
-  var selectedDate = new Date(inputData + 'T' + inputHora); // Data e hora selecionadas
-
-  if (inputNome === "" || inputQtd === "" || inputData === "" || inputHora === "") {
+    var inputNome = encodeURIComponent(document.getElementById('text_nome').value);
+    var inputEvento = encodeURIComponent(document.getElementById('text_evento').value);
+    var inputQtd = encodeURIComponent(document.getElementById('text_qtd').value);
+    var inputHora = document.getElementById('text_hora').value;
+    var inputObs = encodeURIComponent(document.getElementById('text_obs').value);
+  
+    var inputData = document.getElementById('text_data').value;
+    var [ano, mes, dia] = inputData.split("-");
+    var dataFormatada = dia + '/' + mes + '/' + ano;
+  
+    var currentDate = new Date(); // Data e hora atuais
+    var selectedDate = new Date(inputData + 'T' + inputHora); // Data e hora selecionadas
+  
+    if (inputNome === "" || inputQtd === "" || inputData === "" || inputHora === "") {
       alert("Preencha todos os campos obrigatórios");
       return false;
-  }
-
-  if (selectedDate < currentDate) {
+    }
+  
+    if (selectedDate < currentDate) {
       alert("A data e hora não podem ser anteriores à data e hora atuais.");
       return false;
-  }
-
-  if (inputQtd < 2) {
+    }
+  
+    if (inputQtd < 2) {
       alert("Escolha uma quantidade maior que 1");
       return false;
-  }
-
-  if (inputQtd > 30) {
+    }
+  
+    if (inputQtd > 30) {
       alert("Escolha uma quantidade menor que 30 pessoas");
       return false;
-  }
-
-  return true;
+    }
+  
+    var apiLink = "https://api.whatsapp.com/send?phone=+55+55%2012%2098886-5185&text=Olá, gostaria de fazer uma reserva%0A*Nome:*%20" + inputNome + "%0A*Evento:*%20" + inputEvento + "%0A*Quantidade:*%20" + inputQtd + "%20pessoas%0A*Data:*%20" + dataFormatada + "%0A*Horário:*%20" + inputHora + "%0A*Observação:*%20" + inputObs + "";
+    document.getElementById('apiLink').href = apiLink;
+  
+    return true;
 }
-
-function formatarTelefone(event) {
-  var input = event.target;
-  var value = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
-
-  // Formatação com parênteses nos dois primeiros números
-  if (value.length > 2) {
-      value = '(' + value.substr(0, 2) + ') ' + value.substr(2);
-  }
-
-  // Limita o tamanho máximo do número de telefone para 15 dígitos
-  if (value.length > 14) {
-      value = value.substr(0, 14);
-  }
-
-  value = value.replace(/^(\d{2})\s(\d)/, '$1 $2'); // Adiciona espaço após os primeiros 2 dígitos
-  value = value.replace(/(\d{5})(\d)/, '$1-$2'); // Adiciona hífen após os primeiros 5 dígitos
-  input.value = value;
-}
-
-// Evento de submit do formulário
-document.querySelector('form[name="reserva-form"]').addEventListener('submit', function(event) {
-  if (!validarDataHora()) {
-      event.preventDefault(); // Impede o envio do formulário se a validação falhar
-  }
+  
+document.getElementById('apiLink').addEventListener('click', function (event) {
+    if (validarDataHora()) {
+        document.querySelector('form[name="reserva-form"]').submit();
+    } else {
+        event.preventDefault();
+    }
 });
-
-// Evento de input para o campo de telefone
-document.getElementById('text_tel').addEventListener('input', formatarTelefone);
