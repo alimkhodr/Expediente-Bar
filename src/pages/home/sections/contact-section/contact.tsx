6e8bedb36e34f3e8c94185d49596d3312f31d301
alimkhodr <ali.m.khodr@gmail.com>
2025-02-11 14:15:49 -0300

    feat: update Google Maps iframe source in contact section

--- src/pages/home/sections/contact-section/contact.tsx
+++ src/pages/home/sections/contact-section/contact.tsx
@@ -54,7 +54,7 @@ const Contact = () => {
                     </Grid>
                     <Grid item xs={12} md={6}>
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d229.17333992172425!2d-45.908833018473416!3d-23.21492590240116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc35ab7fcf9253%3A0x5b698e4f060e2154!2sPromovere%20Vita%20-%20Coworking!5e0!3m2!1spt-BR!2sbr!4v1736117182280!5m2!1spt-BR!2sbr"
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7333.327207348863!2d-45.887348!3d-23.218927!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc4a8c55544269%3A0xa891d1d896845d41!2sR.%20Paulo%20da%20Silva%20Santos%2C%2032%20-%20Floradas%20de%20S%C3%A3o%20Jos%C3%A9%2C%20S%C3%A3o%20Jos%C3%A9%20dos%20Campos%20-%20SP%2C%2012230-091!5e0!3m2!1spt-BR!2sbr!4v1739294099875!5m2!1spt-BR!2sbr"
                             width="100%"
                             height="300"
                             style={{ border: 0, borderRadius: 5 }}
