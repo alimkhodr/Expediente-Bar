3b4800c0040cbc3177334d2ed2f50a90cfa371de
Ali Mohamed <85517447+alimkhodr@users.noreply.github.com>
2026-06-05 20:57:22 -0300

    fix: adjust publico.vue

--- app/components/tv/publico.vue
+++ app/components/tv/publico.vue
@@ -77,7 +77,7 @@ defineProps<{
   display: flex;
   flex-direction: column;
   gap: 2vmin;
-  padding: 4.5vmin 5vmin;
+  padding: 6.5vmin 7vmin;
   background: #09090b;
   color: #fafafa;
   font-family: 'Poppins', sans-serif;
